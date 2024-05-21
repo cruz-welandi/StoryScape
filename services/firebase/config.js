e1310f40eab4deaac65282d6a836a7fba540149d
cruz-welandi <126161120+cruz-welandi@users.noreply.github.com>
2024-05-21 06:14:00 +0100

    ajustement des bugs et env

--- services/firebase/config.js
+++ services/firebase/config.js
@@ -3,6 +3,13 @@ import  {getAuth} from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+console.log('API Key:', process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+console.log('Auth Domain:', process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
+console.log('Project ID:', process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
+console.log('Storage Bucket:', process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET);
+console.log('Messaging Sender ID:', process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID);
+console.log('App ID:', process.env.NEXT_PUBLIC_FIREBASE_APP_ID);
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
